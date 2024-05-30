765ad79d69da0e22b9bd7365b30b256665ef448d
PaksF <flaksfjtm@gmail.com>
2024-05-30 15:19:11 +0900

    직접 풀어보기 7-3 완성

--- app/src/main/java/kr/ac/kopo/inputuserinformation/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/inputuserinformation/MainActivity.kt
@@ -13,8 +13,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var textName : TextView
-    lateinit var textEmail : TextView
+    lateinit var textName : EditText
+    lateinit var textEmail : EditText
     lateinit var btnDlg : Button
     lateinit var dlgEditName : EditText
     lateinit var dlgEditEmail : EditText
@@ -28,8 +28,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         title = "사용자 정보 입력"
 
-        textName = findViewById<TextView>(R.id.textName)
-        textEmail = findViewById<TextView>(R.id.textEmail)
+        textName = findViewById<EditText>(R.id.textName)
+        textEmail = findViewById<EditText>(R.id.textEmail)
         btnDlg = findViewById<Button>(R.id.btnDlg)
 
         btnDlg.setOnClickListener {
@@ -37,12 +37,15 @@ class MainActivity : AppCompatActivity() {
             var dialog = AlertDialog.Builder(this@MainActivity)
             dialog.setTitle("사용자 정보 입력")
             dialog.setIcon(R.drawable.icon1)
+
+            dlgEditName = dlgView.findViewById<EditText>(R.id.editName)
+            dlgEditEmail = dlgView.findViewById<EditText>(R.id.editEmail)
+            dlgEditName.text = textName.text
+            dlgEditEmail.text = textEmail.text
             dialog.setView(dlgView)
             dialog.setPositiveButton("확인") {dialogL, which ->
-                dlgEditName = dlgView.findViewById<EditText>(R.id.editName)
-                dlgEditEmail = dlgView.findViewById<EditText>(R.id.editEmail)
-                textName.text = dlgEditName.text.toString()
-                textEmail.text = dlgEditEmail.text.toString()
+                textName.text = dlgEditName.text
+                textEmail.text = dlgEditEmail.text
             }
             dialog.setNegativeButton("취소") {dialogL, which ->
                 toastView = View.inflate(this@MainActivity, R.layout.toast, null)
